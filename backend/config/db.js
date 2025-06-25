6ec028c29505666c5c03a35d65c8332d67910f93
ShardulM22 <shardul.mahindrakar22@spit.ac.in>
2025-06-25 21:28:18 +0530

    project added

--- backend/config/db.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async () => {
-    await mongoose.connect('// UR DATABASE WITH ATLAS MONGODB').then(()=>console.log("DataBase Connected"));
-}
